db2b19f69dce8d841608c9a44589f7a7d8ea7e42
eymike <eymike@gmail.com>
2014-03-03 22:03:53 -0800

    Basic lighting.

--- resources/BasicFrag.glsl
+++ resources/BasicFrag.glsl
@@ -1,5 +1,8 @@
 
+varying vec3 viewSpacePosition;
+varying vec3 normal;
+
 void main()
 {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(1, 0, 0, 1) * dot(normal, vec3(0, 1, 0));
 }
--- resources/BasicVertex.glsl
+++ resources/BasicVertex.glsl
@@ -2,9 +2,13 @@ uniform mat4 world;
 uniform mat4 view;
 uniform mat4 proj;
 
-varying vec3 position;
+varying vec3 viewSpacePosition;
+varying vec3 normal;
 
 void main()
 {
-    gl_Position = world * view * proj * gl_Vertex;
+    mat4 viewWorld = view * world;
+    viewSpacePosition = (viewWorld * gl_Vertex).xyz;
+    normal = (viewWorld * vec4(gl_Normal, 0)).xyz;
+    gl_Position = (proj) * vec4(viewSpacePosition, 1);
 }
